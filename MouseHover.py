b540e26b4cdeba02a648031748cbcaf1f8f426d2
unknown <harsh.python.com@gmal.com>
2019-02-16 13:51:58 +0530

    mouse hover,delete cookies,webtable handling,multiplebrowser-env,window handling

---
+++ MouseHover.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+#from selenium.webdriver.common.action_chains import ActionChains
+
+driver=webdriver.Chrome()
+driver.get("file:///C:/Users/QSP/Desktop/webtable.html")
+driver.maximize_window()
+driver.implicitly_wait(30)
+
+ele = driver.find_elements_by_xpath("//*[@id='emp']/thead/tr/th")
+print(len(ele))
+first_part="//*[@id='emp']/thead/tr/th["
+second_part="]"
+
+
+
+
