dacf7e82e02689e1a931372a5fee8922404794da
QSP-fac <harish.python.com@gmail.com>
2019-02-23 13:28:06 +0530

    Added cwd code

--- MouseHover.py
+++ MouseHover.py
@@ -5,7 +5,6 @@ driver=webdriver.Chrome()
 driver.get("file:///C:/Users/QSP/Desktop/webtable.html")
 driver.maximize_window()
 driver.implicitly_wait(30)
-
 ele = driver.find_elements_by_xpath("//*[@id='emp']/thead/tr/th")
 print(len(ele))
 first_part="//*[@id='emp']/thead/tr/th["
---
+++ Rough.py
@@ -0,0 +1,8 @@
+f_x="//*[@id='ui-datepicker-div']/table/tbody/tr/td/a[text()="
+s_x="'"
+e_d="18"
+t_x="'"
+ff_x="]"
+final_x=f_x+s_x+e_d+t_x+ff_x
+
+print(final_x)
